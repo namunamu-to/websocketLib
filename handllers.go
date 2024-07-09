0c1a0da889d71d8c1551e6b988410ef0d8d7fb4a
namunamu-to <ten1baniraSS@gmail.com>
2024-07-09 17:19:43 +0900

    ハンドラ名変更

--- handllers.go
+++ handllers.go
@@ -3,12 +3,12 @@ package main
 import "strconv"
 
 //オウム返し
-// func test1(plData player, msg string) {
+// func handllerEx1(plData player, msg string) {
 // 	sendMsg(plData.conn, msg)
 // }
 
 // 特定のコマンドなら、オウム返しする
-func test2(plData player, msg string) {
+func handllerEx2(plData player, msg string) {
 	cmd, cmdType, cmdLen := readCmd(msg)
 	if cmdType == "test1" && cmdLen == 2 {
 		sendMsg(plData.conn, cmd[1])
@@ -16,7 +16,7 @@ func test2(plData player, msg string) {
 }
 
 // 挨拶を返す
-func test3(plData player, msg string) {
+func handllerEx3(plData player, msg string) {
 	if msg != "hello" {
 		return
 	}
@@ -25,7 +25,7 @@ func test3(plData player, msg string) {
 }
 
 // プレイヤーデータを用いてルーム情報にアクセスし、クライアントに返す
-func test4(plData player, msg string) {
+func handllerEx4(plData player, msg string) {
 	_, cmdType, _ := readCmd(msg)
 	if cmdType != "roomInfo" {
 		return
@@ -41,8 +41,8 @@ func test4(plData player, msg string) {
 }
 
 func SetHandllers() {
-	// addedHandllers = append(addedHandllers, test1)
-	addedHandllers = append(addedHandllers, test2)
-	addedHandllers = append(addedHandllers, test3)
-	addedHandllers = append(addedHandllers, test4)
+	// addedHandllers = append(addedHandllers, handllerEx1)
+	addedHandllers = append(addedHandllers, handllerEx2)
+	addedHandllers = append(addedHandllers, handllerEx3)
+	addedHandllers = append(addedHandllers, handllerEx4)
 }
